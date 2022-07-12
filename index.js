7be2bc8d8eeb6eb0fb8ff5536670a8c6955e7a4b
TXMAY <47449096+TXMAY@users.noreply.github.com>
2022-07-12 09:42:14 +0900

    init

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!!! lol");
+  res.send("Hello World!!! yee");
 });
 
 app.get("/goodbye", (req, res) => {
